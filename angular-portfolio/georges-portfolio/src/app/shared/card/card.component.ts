618e029f1bcdc8bf5c7c49229dbdb3f022b2669a
Georges FARDISSI <fardissi.georges@gmail.com>
2025-03-26 17:07:48 +0200

    Final changes

--- angular-portfolio/georges-portfolio/src/app/shared/card/card.component.ts
+++ angular-portfolio/georges-portfolio/src/app/shared/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+@Input() img: string ='';
+@Input() imgtxt: string ='';
+@Input() title: string ='';
 }
